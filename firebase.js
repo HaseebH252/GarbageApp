138184b1a936368b3c56f668dff3f5982371102c
Ausarion <rja36@njit.edu>
2019-04-02 23:27:45 -0400

    Update Firebase.js
    Just needed to fill in information for api key etc.

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 import * as firebase from 'firebase';
 const config = {
-  apiKey: "ENTER YOURS HERE",
-  authDomain: "ENTER YOURS HERE",
-  databaseURL: "ENTER YOURS HERE",
-  projectId: "ENTER YOURS HERE",
-  storageBucket: "ENTER YOURS HERE",
-  messagingSenderId: "ENTER YOURS HERE"
+  apiKey: "AIzaSyAhqPILYiQz4-f5pIy3lYXz-GIDYqTZcuI",
+  authDomain: "https://garbageapp-d3bd6.firebaseapp.com/",
+  databaseURL: "http://garbageapp-d3bd6.firebaseio.com",
+  projectId:"garbageapp-d3bd6",
+  storageBucket: "gs://garbageapp-d3bd6.appspot.com",
+  messagingSenderId: "347169073887"
 }
 firebase.initializeApp(config);
 const databaseRef = firebase.database().ref();
